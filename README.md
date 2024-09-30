dc59aedc3928d5c491bb4ab6a586f0a7639cd87e
devsembo <anderson.ap01@hotmail.com>
2024-09-30 19:18:43 +0100

    chore: update home page

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 - {  } configuração das paginas de login (usuario e Adms);
 - {  } Criação do fluxo de agendamentos;
 - { } conexão com api;
+
+
+
+# Para  testar
+faça o clone deste repositorio no seu pc:
+execute os seguintes comandos no seu terminal:
+
+`git clone https://github.com/devsembo/cutify-webapp.git`;
+
+a seguir execute: `yarn instal` para instalar todas as dependecias.
+
+e finalmente com o seu pc conectado a internet, execute: `yarn dev -H endereço ip da sua maquina -p 3000`;
+
+para saber o ip da sua maquina execute no seu terminal: `ipconfig` se for windows e `ifconfig` se for linux;
+
+obter provavelmente duas possiveis portas:
+-Network: 192.168.1.254 (exempo)
+-local: 192.168.1.254 (exempos)
+
+execute estes numeros no seu telemovel.
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import banner from "../../public/assets/banner.png"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar"
 export default function Home() {
   return (
     <>
@@ -15,6 +18,7 @@ export default function Home() {
         </h2>
         <p>Segunda-feira, 30 de Outubro.</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Pesquisa" className="text-white" />
           <Button size="icon">
@@ -22,6 +26,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* Imagem */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src={banner}
@@ -30,6 +35,36 @@ export default function Home() {
             alt="Agnede com os melhores com a cutify"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <div className="flex"></div>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="h-6 w-fit bg-green-100 text-green-600">
+                Confirmado
+              </Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-10 w-10 overflow-hidden rounded-full">
+                  <AvatarImage
+                    src="http://localhost:3333/image/e7139d5ad3-Captura de ecrÃ£ 2024-09-29 203401.png"
+                    alt="Mike Pina Barbeiro da Classic BarberShop"
+                  />
+                  <AvatarFallback>CB</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Classic BarberShop</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Outubro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">18:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   )
