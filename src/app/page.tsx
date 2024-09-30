4ab9f043a520f996c01a609f8302dae0d19a6326
devsembo <anderson.ap01@hotmail.com>
2024-09-30 15:25:30 +0100

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-"use client"
 import React from 'react'
+import { Button } from './_components/ui/button'
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-red-500 p-12 mb-5 bg-white'>Home Page</h1>
+      <Button>Teste</Button>
     </>
   )
 }
