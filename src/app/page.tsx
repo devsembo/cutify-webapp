b236dcfb3fe211bbc1e244de8ea994a67b5773a2
devsembo Anderson Pedro <anderson.ap01@icloud.com>
2024-10-01 19:08:29 +0100

    feat: add comendados and popular section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,18 @@ import banner from "../../public/assets/banner.png"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar"
+import barber from "../../public/assets/classic.jpg"
+import OtherBarber from "../../public/assets/barber.jpg"
+
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="p-5">
+      <div className="mt-4">
         <h2>
           Olá, <span className="text-xl font-bold">Anderson!</span>
         </h2>
-        <p>Segunda-feira, 30 de Outubro.</p>
+        <p>Segunda-feira, 01 de Outubro.</p>
 
         {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
@@ -38,7 +41,7 @@ export default function Home() {
 
         {/* AGENDAMENTO */}
         <div className="flex"></div>
-        <Card className="mt-6">
+        <Card className="mt-6 bg-gray-950">
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="h-6 w-fit bg-green-100 text-green-600">
@@ -65,6 +68,266 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+
+        {/* Reconmendados */}
+        <h4 className="pt-5 text-gray-600">RECOMENDADOS</h4>
+        <div className="flex w-full flex-row gap-8 overflow-scroll pb-8">
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={barber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                Classic BarberShop
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+        </div>
+
+        {/* POPULARES */}
+        <h4 className="pt-5 text-gray-600">POPULARES</h4>
+        <div className="flex w-full flex-row gap-8 overflow-scroll pb-8">
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-6 h-72 w-48 bg-gray-950">
+            <CardContent className="flex w-48 flex-col justify-center gap-1 p-0">
+              <Image
+                src={OtherBarber}
+                alt="Classic BarberShop "
+                width={500}
+                height={300}
+                className="h-40 rounded-2xl object-cover p-2"
+              />
+              <p className="ml-1 text-sm font-semibold text-white">
+                El Peloquero
+              </p>
+              <p className="p-1 text-xs text-gray-500">
+                Rua de Fernandes Tomás 121, Porto.
+              </p>
+              <Badge className="ml-10 flex h-8 w-28 items-center justify-center bg-gray-500 text-base text-white">
+                Reservar
+              </Badge>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </>
   )
